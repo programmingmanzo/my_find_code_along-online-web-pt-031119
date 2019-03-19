574fed16ead93782441758df8674291316d76188
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-19 09:26:01 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -11,3 +11,13 @@ def my_find(collection)
     i += 1
   end
 end
+
+def my_find(collection)
+  i = 0 
+  while i < collection.length 
+    if yield(collection[i])
+      return collection[i]
+    end
+    i += 1
+  end
+end
